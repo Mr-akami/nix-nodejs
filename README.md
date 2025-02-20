6039a4d604ca69a94bf10ece68ac145511129bb0
shun murakami <shun_murakami@earthbrain.com>
2025-02-20 23:05:42 +0900

    deno build

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ https://zenn.dev/stmn_inc/articles/create-environment-to-nix-flake
   - . はローカルの flake.nix を指定
 
 nix develop すると bash が開かれる。host の zsh を引き継ぎたいけどどうやるんだっけ。毎回やり方を忘れている。
+
+sha256 の取得
+
+1. nix-prefetch-url --unpack https://github.com/denoland/deno/releases/download/v2.2.0/deno-x86_64-unknown-linux-gnu.zip
+2. SRI 形式にする
+   nix hash to-sri --type sha256 13asw18wvi33lh1cqx79d24rrx839mfb23y8pv0dhy7qd1npb01a
+
+ビルドを失敗しても SRI が出るので上記は必要ない
--- environments/deno/shell.nix
+++ environments/deno/shell.nix
@@ -3,6 +3,33 @@
 let
   # 共通の設定を読み込む
   baseShell = import ../../shells/shell.nix { inherit packages; };
+  # denoをビルド
+  denoLatest = packages.deno.overrideAttrs (oldAttrs: {
+    version = "2.2.0";
+    src = packages.fetchurl {
+      url = "https://github.com/denoland/deno/releases/download/v2.2.0/deno-x86_64-unknown-linux-gnu.zip";
+      sha256 = "sha256-KoB1bWj4eNjAvsgPsVxNA/WciWjpdMwCpGPEzVHgWo0=";
+    };
+  });
+
+  # denoLatest = packages.stdenv.mkDerivation rec {
+  #   pname = "deno";
+  #   version = "1.40.1"; # 任意のDenoバージョン
+
+  #   src = packages.fetchFromGitHub {
+  #     owner = "denoland";
+  #     repo = "deno";
+  #     rev = "v2.2.0";  # GitHubのタグを指定
+  #     sha256 = "13asw18wvi33lh1cqx79d24rrx839mfb23y8pv0dhy7qd1npb01a"; # `nix-prefetch-url` で取得
+  #   };
+
+  #   buildInputs = [ packages.cmake packages.rustc packages.cargo ];
+
+  #   installPhase = ''
+  #     mkdir -p $out/bin
+  #     cp target/release/deno $out/bin/
+  #   '';
+  # };
 in
 packages.mkShell {
   # 基本シェルから設定を継承
@@ -10,7 +37,7 @@ packages.mkShell {
 
   # 基本シェルから buildInputs を継承
   buildInputs = baseShell.buildInputs ++ (with packages; [
-    deno # deno latest
+    denoLatest # deno latest
     nodePackages.pnpm # pnpmを追加
   ]);
 
