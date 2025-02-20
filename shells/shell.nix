1686464ff8d12b021672fd63a5b01b56a804169b
shun murakami <shun_murakami@earthbrain.com>
2025-02-20 22:28:41 +0900

    initial

---
+++ shells/shell.nix
@@ -0,0 +1,17 @@
+{ packages ? import <nixpkgs> {} }:
+
+packages.mkShell {
+  # 基本的な開発ツール
+  buildInputs = with packages; [
+    # git
+    # docker
+  ];
+
+  # ホストの環境と完全に分離する
+  # この指定がないと、ホスト側の環境を継承するので継承をさせないための設定
+  # pure = true;
+
+  shellHook = ''
+    echo " Welcome to nix-craft development environment"
+  '';
+}
